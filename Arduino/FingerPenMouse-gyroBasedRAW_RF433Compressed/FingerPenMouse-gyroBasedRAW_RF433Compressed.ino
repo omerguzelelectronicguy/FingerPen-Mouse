093cc77d614aef7a3ceccceb1e7c00d9b050067d
omerguzelelectronicguy <omer.guzel@agu.edu.tr>
2022-01-08 15:42:20 +0300

    RF programs created
    RF 433 was one of the wireless alternatives that should be tried. these are to try it. it has low data rate that makes problem.

---
+++ Arduino/FingerPenMouse-gyroBasedRAW_RF433Compressed/FingerPenMouse-gyroBasedRAW_RF433Compressed.ino
@@ -0,0 +1,295 @@
+// I2C device class (I2Cdev) demonstration Arduino sketch for MPU6050 class
+// 10/7/2011 by Jeff Rowberg <jeff@rowberg.net>
+// Updates should (hopefully) always be available at https://github.com/jrowberg/i2cdevlib
+//
+// Changelog:
+//      2013-05-08 - added multiple output formats
+//                 - added seamless Fastwire support
+//      2011-10-07 - initial release
+
+/* ============================================
+I2Cdev device library code is placed under the MIT license
+Copyright (c) 2011 Jeff Rowberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
+unsigned long t;
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include <RH_ASK.h> // Include RadioHead Amplitude Shift Keying Library
+#include <SPI.h> // Include dependant SPI Library
+RH_ASK rf_driver;
+
+
+const int8_t compress_var[] {5,7,9,13,21,37,69,133,255};
+bool x_sign = 1;
+bool y_sign = 1;
+int8_t compress(uint8_t xvar){
+  uint8_t res;
+  
+  if(xvar<compress_var[0]){//5
+    res = res & xvar;
+  }else if(xvar<compress_var[1]){
+    res = 6;
+  }else if(xvar<compress_var[2]){
+    res = 7;
+  }else if(xvar<compress_var[3]){
+    res = 8;
+  }else if(xvar<compress_var[4]){
+    res = 9;
+  }else if(xvar<compress_var[5]){
+    res = 10;
+  }else if(xvar<compress_var[6]){
+    res = 11;
+  }else if(xvar<compress_var[7]){
+    res = 12;
+  }else if(xvar<compress_var[8]){
+    res = 13;
+  }else if (xvar==compress_var[8]){
+    res = 14;
+  }
+  return res;
+}
+
+uint8_t compress2bit(uint8_t a,uint8_t b){
+  uint8_t res;
+  res = compress(a);
+  res = res << 4;
+  res = res | compress(b);
+  return res;
+}
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 accelgyro;
+//MPU6050 accelgyro(0x69); // <-- use for AD0 high
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+#define MOUSEEVENTF_MOVE 0x0001
+#define MOUSEEVENTF_LEFTDOWN 0x0002
+#define MOUSEEVENTF_LEFTUP 0x0004
+#define MOUSEEVENTF_RIGHTDOWN 0x0008
+#define MOUSEEVENTF_RIGHTUP 0x0010
+#define MOUSEEVENTF_MIDDLEDOWN 0x0020
+#define MOUSEEVENTF_MIDDLEUP 0x0040
+#define button1 3
+#define button2 A7
+#define baudRate 1000000
+
+bool btn1 = 0;
+byte buttonState1 = MOUSEEVENTF_LEFTUP;
+byte oldButtonState1 = buttonState1;
+bool btn2 = 0;
+byte buttonState2 = MOUSEEVENTF_RIGHTUP;
+byte oldButtonState2 = buttonState2;
+
+
+// uncomment "OUTPUT_READABLE_ACCELGYRO" if you want to see a tab-separated
+// list of the accel X/Y/Z and then gyro X/Y/Z values in decimal. Easy to read,
+// not so easy to parse, and slow(er) over UART.
+#define OUTPUT_READABLE_ACCELGYRO
+
+// uncomment "OUTPUT_BINARY_ACCELGYRO" to send all 6 axes of data as 16-bit
+// binary, one right after the other. This is very fast (as fast as possible
+// without compression or data loss), and easy to parse, but impossible to read
+// for a human.
+//#define OUTPUT_BINARY_ACCELGYRO
+
+
+#define LED_PIN 13
+bool blinkState = false;
+
+void setup() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    // initialize serial communication
+    // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
+    // it's really up to you depending on your project)
+    Serial.begin(baudRate);
+    rf_driver.init();
+
+    // initialize device
+    //Serial.println("Initializing I2C devices...");
+    accelgyro.initialize();
+
+    // verify connection
+    //Serial.println("Testing device connections...");
+    //Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+
+    // use the code below to change accel/gyro offset values
+    
+    //Serial.println("Updating internal sensor offsets...");
+    //-2466  -211  1553  -205  -101  35  
+    //-2466 -211  1553  220 76  -85 
+    /*Serial.print(accelgyro.getXAccelOffset()); Serial.print("\t"); // -76
+    Serial.print(accelgyro.getYAccelOffset()); Serial.print("\t"); // -2359
+    Serial.print(accelgyro.getZAccelOffset()); Serial.print("\t"); // 1688
+    Serial.print(accelgyro.getXGyroOffset()); Serial.print("\t"); // 0
+    Serial.print(accelgyro.getYGyroOffset()); Serial.print("\t"); // 0
+    Serial.print(accelgyro.getZGyroOffset()); Serial.print("\t"); // 0
+    Serial.print("\n");
+    accelgyro.setXGyroOffset(-205);
+    accelgyro.setYGyroOffset(-101);
+    accelgyro.setZGyroOffset(35);
+    accelgyro.setXAccelOffset(-2466);
+    accelgyro.setYAccelOffset(-211);
+    accelgyro.setZAccelOffset(1553);
+    Serial.print(accelgyro.getXAccelOffset()); Serial.print("\t"); // -76
+    Serial.print(accelgyro.getYAccelOffset()); Serial.print("\t"); // -2359
+    Serial.print(accelgyro.getZAccelOffset()); Serial.print("\t"); // 1688
+    Serial.print(accelgyro.getXGyroOffset()); Serial.print("\t"); // 0
+    Serial.print(accelgyro.getYGyroOffset()); Serial.print("\t"); // 0
+    Serial.print(accelgyro.getZGyroOffset()); Serial.print("\t"); // 0
+    Serial.print("\n");*/
+    //-2466  -211  1553  -205  -101  35  
+    //-2466 -211  1553  220 76  -85 
+    
+    accelgyro.setXGyroOffset(-205);
+    accelgyro.setYGyroOffset(-101);
+    accelgyro.setZGyroOffset(35);
+    accelgyro.setXAccelOffset(-2466);
+    accelgyro.setYAccelOffset(-211);
+    accelgyro.setZAccelOffset(1553);
+    
+    // configure Arduino LED pin for output
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(button1, INPUT_PULLUP);
+    pinMode(button2, INPUT_PULLUP);
+
+  byte message = {85};//to start the communication.
+  Serial.write((char *)&message, sizeof(message));
+  Serial.write((char *)&message, sizeof(message));
+  Serial.write((char *)&message, sizeof(message));
+  Serial.write((char *)&message, sizeof(message));
+  t= micros();
+}
+byte a =0;
+void loop() {
+    buttonState1 = digitalRead(button1) ? MOUSEEVENTF_LEFTDOWN : MOUSEEVENTF_LEFTUP ;
+    //buttonState2 = digitalRead(button2)? MOUSEEVENTF_RIGHTDOWN : MOUSEEVENTF_RIGHTUP ;;
+    
+    if(buttonState1!=oldButtonState1){
+      byte message[] = {(int8_t)oldButtonState1,0,0};
+      oldButtonState1 = buttonState1;
+      //int message[] = {(int)buttonState1, 0, 0, 0};
+      Serial.write((char *)&message, sizeof(message));
+      
+      //rf_driver.send((int8_t *)message, sizeof(message));
+      //rf_driver.waitPacketSent();
+    }
+    /*if(buttonState2!=oldButtonState2){
+      oldButtonState2 = buttonState2;
+      int message[] = {(int)buttonState2, 0, 0, 0};
+      Serial.write((char *)&message, sizeof(message));
+    }*/
+      
+    
+   
+
+  /*a++;
+  if (a==255){
+    t= micros()-t;
+    Serial.println();
+    Serial.println(t);
+    t= micros();
+  }*/
+    // read raw accel/gyro measurements from device
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+    // these methods (and a few others) are also available
+    //accelgyro.getAcceleration(&ax, &ay, &az);
+    //accelgyro.getRotation(&gx, &gy, &gz);
+
+    #ifdef OUTPUT_READABLE_ACCELGYRO
+        // display tab-separated accel/gyro x/y/z values
+      //int message[] = {(int) 1, -(int)gz/512, -(int)gx/512, (int)gy/512};
+      int8_t dx = (int8_t)(gx/256);
+      int8_t dy = (int8_t)(gz/256);
+      
+      if(dx != 0 || dy != 0){
+        int8_t a;
+        if(dx>0){
+          if(dy>0){
+            a = compress2bit(x_sign ? dx : 255 ,y_sign ? dy : 255);
+            y_sign=1;
+          }else{            
+            a = compress2bit(x_sign ? dx : 255 ,!y_sign ? dy : 255);
+            y_sign=0;
+          }
+          x_sign = 1;
+        }else{
+          if(dy>0){
+            a = compress2bit(!x_sign ? dx : 14 ,y_sign ? dy : 255);
+            y_sign = 1;
+          }else{
+            a = compress2bit(!x_sign ? dx : 255 ,!y_sign ? dy : 255);
+            y_sign = 0;
+          }
+            x_sign = 0;
+        }
+        
+        //delay(25);
+        //Serial.write((char *)&a, sizeof(a));
+        rf_driver.send((char *)a, sizeof(a));
+        rf_driver.waitPacketSent();
+
+
+      }
+      delayMicroseconds(100);
+     
+       /* Serial.print("a/g:\t");
+        Serial.print(ax); Serial.print("\t");
+        Serial.print(ay); Serial.print("\t");
+        Serial.print(az); Serial.print("\t");
+        Serial.print(gx/128); Serial.print("\t");
+        Serial.print(gy/128); Serial.print("\t");
+        Serial.println(gz/128);*/
+    #endif
+
+    #ifdef OUTPUT_BINARY_ACCELGYRO
+        Serial.write((uint8_t)(ax >> 8)); Serial.write((uint8_t)(ax & 0xFF));
+        Serial.write((uint8_t)(ay >> 8)); Serial.write((uint8_t)(ay & 0xFF));
+        Serial.write((uint8_t)(az >> 8)); Serial.write((uint8_t)(az & 0xFF));
+        Serial.write((uint8_t)(gx >> 8)); Serial.write((uint8_t)(gx & 0xFF));
+        Serial.write((uint8_t)(gy >> 8)); Serial.write((uint8_t)(gy & 0xFF));
+        Serial.write((uint8_t)(gz >> 8)); Serial.write((uint8_t)(gz & 0xFF));
+    #endif
+
+    // blink LED to indicate activity
+    blinkState = !blinkState;
+    digitalWrite(LED_PIN, blinkState);
+}
